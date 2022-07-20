ce006d579ce8bb7afe4ff4b2f39a75c7fb478b75
AlexeyZabalotcki <AlexeyZabalotckiBusiness@gmail.com>
2022-07-20 18:23:39 +0300

    Created a new project by Spring Boot

---
+++ src/main/java/com/example/productdbtesttask/ProductDbTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.productdbtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDbTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductDbTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/productdbtesttask/ProductDbTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productdbtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDbTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
